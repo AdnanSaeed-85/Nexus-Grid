aa5372f150622093f675dc740e275ba2f30c25ee
AdnanSaeed-85 <saeedadnan151@gmail.com>
2026-09-01 22:15:16 +0500

    db creating

--- backend/create_db.py
+++ backend/create_db.py
@@ -1,8 +1,8 @@
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
+from sqlalchemy import create_engine
 from dotenv import load_dotenv
 from pathlib import Path
+import os
 
 load_dotenv(Path(__file__).parent / ".env")
 
